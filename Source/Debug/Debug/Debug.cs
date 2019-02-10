ab9705244b2a9bee8dbd5194dd40f6883e942646
erdelf <erdelf.productions@gmail.com>
2019-02-10 15:55:40 +0100

    smaller renamed humans

--- Source/Debug/Debug/Debug.cs
+++ Source/Debug/Debug/Debug.cs
@@ -20,14 +20,17 @@ namespace Debug
     {
         static Debug()
         {
-            HarmonyInstance harmony = HarmonyInstance.Create(id: "rimworld.erdelf.debug");
-            harmony.Patch(AccessTools.Method(typeof(HediffMaker), nameof(HediffMaker.MakeHediff)), new HarmonyMethod(typeof(Debug), nameof(Prefix)));
+            //HarmonyInstance harmony = HarmonyInstance.Create(id: "rimworld.erdelf.debug");
+            //harmony.Patch(AccessTools.Method(typeof(HediffMaker), nameof(HediffMaker.MakeHediff)), new HarmonyMethod(typeof(Debug), nameof(Prefix)));
+            typeof(MeshPool).GetField("humanlikeBodySet", BindingFlags.Public | BindingFlags.Static).SetValue(null, new GraphicMeshSet(0.75f));
+            typeof(MeshPool).GetField("humanlikeHeadSet", BindingFlags.Public | BindingFlags.Static).SetValue(null, new GraphicMeshSet(0.75f));
+            typeof(MeshPool).GetField("humanlikeHairSetAverage", BindingFlags.Public | BindingFlags.Static).SetValue(null, new GraphicMeshSet(0.75f));
+            typeof(MeshPool).GetField("humanlikeHairSetNarrow", BindingFlags.Public | BindingFlags.Static).SetValue(null, new GraphicMeshSet(0.9f, 0.75f));
 
-        }
+            foreach (BodyTypeDef bodyTypeDef in DefDatabase<BodyTypeDef>.AllDefs)
+                bodyTypeDef.headOffset.y = 0.2f;
 
-        public static void Prefix(HediffDef td)
-        {
-            Log.Message($"Generating {td?.defName ?? "TD null"} from {td?.modContentPack?.Name ?? "Mod null"}", true);
+            ThingDefOf.Human.label = "homo floresiensis";
         }
     }
 
