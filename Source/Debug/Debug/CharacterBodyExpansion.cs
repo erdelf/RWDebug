a8caedc77ab1f9fd329318a6948f56f59577e23a
erdelf <erdelf.productions@gmail.com>
2019-05-20 20:16:29 +0200

    CharacterBodyExtension

---
+++ Source/Debug/Debug/CharacterBodyExpansion.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace CharacterBodyExpansion
+{
+    using System.Reflection;
+    using System.Reflection.Emit;
+
+    [StaticConstructorOnStartup]
+    public class CharacterBodyExpansion
+    {
+        static CharacterBodyExpansion()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.CharacterBodyExpansion");
+            harmony.Patch(AccessTools.Method(typeof(ApparelGraphicRecordGetter), nameof(ApparelGraphicRecordGetter.TryGetGraphicApparel)), transpiler: new HarmonyMethod(typeof(CharacterBodyExpansion), nameof(CharacterBodyExpansion.Transpiler)));
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            FieldInfo defInfo = AccessTools.Field(typeof(Def), nameof(Def.defName));
+
+            foreach (CodeInstruction instruction in instructions)
+            {
+
+                if (instruction.opcode == OpCodes.Ldfld && instruction.operand == defInfo)
+                    yield return new CodeInstruction(OpCodes.Call, operand: AccessTools.Method(typeof(CharacterBodyExpansion), nameof(GetApparelBodyDefname)));
+                else
+                    yield return instruction;
+            }
+
+        }
+
+        public static string GetApparelBodyDefname(Def def) => 
+            def.GetModExtension<BodyDefApparelParentageInfo>()?.parent.defName ?? def.defName;
+    }
+
+    public class BodyDefApparelParentageInfo : DefModExtension
+    {
+        public BodyTypeDef parent;
+    }
+}
