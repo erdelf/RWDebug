e6059fe6249fee273d95f1d018bbccfaa5fb0cfe
Simon Latusek <simon.latusek@gmail.com>
2020-04-15 12:57:41 +0200

    CanUseApparel + 1.1 Update

--- Source/Debug/Debug/Debug.cs
+++ Source/Debug/Debug/Debug.cs
@@ -1,5 +1,4 @@
-﻿using Harmony;
-using RimWorld;
+﻿using RimWorld;
 using System;
 using Verse;
 
@@ -11,9 +10,9 @@ namespace Debug
     using System.Linq;
     using System.Reflection;
     using System.Reflection.Emit;
+    using HarmonyLib;
     using JetBrains.Annotations;
     using RimWorld.Planet;
-    using TMPro;
     using UnityEngine;
     using Object = System.Object;
 
@@ -22,37 +21,19 @@ namespace Debug
     {
         static Debug()
         {
-             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.debug");
+             Harmony harmony = new Harmony("rimworld.erdelf.debug");
 
 
 
-             HarmonyInstance.DEBUG = true;
-            harmony.Patch(AccessTools.Method(typeof(InspectGizmoGrid), nameof(InspectGizmoGrid.DrawInspectGizmoGridFor)), transpiler: new HarmonyMethod(typeof(Debug), nameof(Transpiler)));
+             //HarmonyInstance.DEBUG = true;
+             harmony.Patch(AccessTools.Method(typeof(PawnApparelGenerator), "CanUsePair"), prefix: new HarmonyMethod(typeof(Debug), nameof(Debug.Prefix)));
 
         }
 
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        public static void Prefix(ThingStuffPair pair, Pawn pawn)
         {
-            List<CodeInstruction> instructionList = instructions.ToList();
-            MethodInfo clearInfo = AccessTools.Method(typeof(List<object>), nameof(List<object>.Clear));
-
-            for (int i = 0; i < instructionList.Count; i++)
-            { 
-                CodeInstruction instruction = instructionList[i];
-
-                yield return instruction;
-
-                if (instruction.opcode == OpCodes.Ldsfld && i > 0 && instructionList[i - 1].operand == clearInfo && instructionList[i+1].opcode == OpCodes.Call)
-                {
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Debug), nameof(GizmosCheck)));
-                }
-            }
-        }
-
-        public static List<Gizmo> GizmosCheck(List<Gizmo> gizmos, IEnumerable<object> selected)
-        {
-            return gizmos;
+            Log.Message($"{pair.thing?.defName ?? "NO DEF"} {pair.thing?.modContentPack.PackageId ?? "NO MOD"}\n{pair.stuff?.defName ?? "NO STUFF"} {pair.stuff?.modContentPack.PackageId ?? "NO MOD"}\n{pawn != null} {pawn?.Name?.ToStringFull ?? "NO NAME"} {pawn?.def?.label}", true);
+            Log.ResetMessageCount();
         }
     }
 
