68ad70c598a3c4688a6b67ebfb9a23d7bf2ede22
Simon Latusek <simon.latusek@gmail.com>
2020-04-19 13:59:22 +0200

    export texture from game to desktop

--- Source/Debug/Debug/Debug.cs
+++ Source/Debug/Debug/Debug.cs
@@ -4,12 +4,14 @@ using Verse;
 
 namespace Debug
 {
+    using System.Collections;
     using System.Collections.Generic;
     using System.ComponentModel;
     using System.IO;
     using System.Linq;
     using System.Reflection;
     using System.Reflection.Emit;
+    using System.Threading;
     using HarmonyLib;
     using JetBrains.Annotations;
     using RimWorld.Planet;
@@ -21,13 +23,37 @@ namespace Debug
     {
         static Debug()
         {
-             Harmony harmony = new Harmony("rimworld.erdelf.debug");
+            Harmony harmony = new Harmony("rimworld.erdelf.debug");
 
 
 
-             //HarmonyInstance.DEBUG = true;
-             harmony.Patch(AccessTools.Method(typeof(CompArt), nameof(CompArt.GenerateImageDescription)), prefix: new HarmonyMethod(typeof(Debug), nameof(Debug.Prefix)));
+            //HarmonyInstance.DEBUG = true;
+            //harmony.Patch(AccessTools.Method(typeof(CompArt), nameof(CompArt.GenerateImageDescription)), prefix: new HarmonyMethod(typeof(Debug), nameof(Debug.Prefix)));
+            Log.Message("hey");
 
+
+
+            Texture2D mainTexture = ContentFinder<Texture2D>.Get("image path");
+            RenderTexture tmp2 = RenderTexture.GetTemporary(
+                                                            mainTexture.width,
+                                                            mainTexture.height,
+                                                            0,
+                                                            RenderTextureFormat.Default,
+                                                            RenderTextureReadWrite.Linear);
+            Graphics.Blit(mainTexture, tmp2);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = tmp2;
+            Texture2D myTexture2D = new Texture2D(mainTexture.width, mainTexture.height);
+            myTexture2D.ReadPixels(new Rect(0, 0, tmp2.width, tmp2.height), 0, 0);
+            myTexture2D.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(tmp2);
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\texture.png";
+
+            System.IO.Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+            System.IO.File.WriteAllBytes(path, myTexture2D.EncodeToPNG());
         }
 
         public static bool Prefix(CompArt __instance, ref TaggedString __result)
