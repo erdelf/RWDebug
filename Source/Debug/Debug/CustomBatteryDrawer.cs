e7f1f7eec0f9e6b6f569b83a657866f69d27660c
Simon Latusek <simon.latusek@gmail.com>
2020-05-09 17:10:31 +0200

    custom battery

---
+++ Source/Debug/Debug/CustomBatteryDrawer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomBatteryDrawer
+{
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+
+    [StaticConstructorOnStartup]
+    class Building_CustomBatteryDrawer : Building_Battery
+    {
+        private static readonly Material batteryBarFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.9f, 0.85f, 0.2f));
+
+        private static readonly Material batteryBarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.3f, 0.3f, 0.3f));
+
+        //[TweakValue("CustomBatteryDrawPosX", -2, 2)]
+        private static float CustomBatteryDrawPosX = -0.51f;
+
+        //[TweakValue("CustomBatteryDrawPosY", -2, 2)]
+        private static float CustomBatteryDrawPosY = -0.695f;
+
+        //[TweakValue("CustomBatteryDrawSizeX", -3, 3)]
+        private static float CustomBatteryDrawSizeX = 0.72f;
+
+        //[TweakValue("CustomBatteryDrawSizeY", -3, 3)]
+        private static float CustomBatteryDrawSizeY = 0.28f;
+
+
+        public override void Draw()
+        {
+            DrawAt(DrawPos);
+            CompPowerBattery           comp = GetComp<CompPowerBattery>();
+            GenDraw.FillableBarRequest r    = default(GenDraw.FillableBarRequest);
+            r.center      = DrawPos + Vector3.up * 0.1f + new Vector3(CustomBatteryDrawPosX, 0, CustomBatteryDrawPosY);
+            r.size        = new Vector2(CustomBatteryDrawSizeX, CustomBatteryDrawSizeY);
+            r.fillPercent = comp.StoredEnergy / comp.Props.storedEnergyMax;
+            r.filledMat   = batteryBarFilledMat;
+            r.unfilledMat = batteryBarUnfilledMat;
+            r.margin      = 0.15f;
+            Rot4 rotation = base.Rotation;
+            rotation.Rotate(RotationDirection.Clockwise);
+            r.rotation = rotation;
+            GenDraw.DrawFillableBar(r);
+            //base.Draw();
+        }
+    }
+}
--- Source/Debug/Debug/Debug.cs
+++ Source/Debug/Debug/Debug.cs
@@ -1,7 +1,7 @@
 ﻿using RimWorld;
 using System;
 using Verse;
-
+/*
 namespace Debug
 {
     using System.Collections;
@@ -28,57 +28,44 @@ namespace Debug
 
 
             //HarmonyInstance.DEBUG = true;
-            //harmony.Patch(AccessTools.Method(typeof(CompArt), nameof(CompArt.GenerateImageDescription)), prefix: new HarmonyMethod(typeof(Debug), nameof(Debug.Prefix)));
-            Log.Message("hey");
+            harmony.Patch(AccessTools.Method(typeof(ThingDef), nameof(ThingDef.SpecialDisplayStats)), postfix: new HarmonyMethod(typeof(Debug), nameof(Debug.Postfix)));
+        }
 
 
 
-            Texture2D mainTexture = ContentFinder<Texture2D>.Get("image path");
-            RenderTexture tmp2 = RenderTexture.GetTemporary(
-                                                            mainTexture.width,
-                                                            mainTexture.height,
-                                                            0,
-                                                            RenderTextureFormat.Default,
-                                                            RenderTextureReadWrite.Linear);
-            Graphics.Blit(mainTexture, tmp2);
-            RenderTexture previous = RenderTexture.active;
-            RenderTexture.active = tmp2;
-            Texture2D myTexture2D = new Texture2D(mainTexture.width, mainTexture.height);
-            myTexture2D.ReadPixels(new Rect(0, 0, tmp2.width, tmp2.height), 0, 0);
-            myTexture2D.Apply();
-            RenderTexture.active = previous;
-            RenderTexture.ReleaseTemporary(tmp2);
-
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\texture.png";
-
-            System.IO.Directory.CreateDirectory(Path.GetDirectoryName(path));
-
-            System.IO.File.WriteAllBytes(path, myTexture2D.EncodeToPNG());
+        public static IEnumerable<StatDrawEntry> Postfix(IEnumerable<StatDrawEntry> __result, ThingDef __instance, StatRequest req)
+        {
+            Log.Error("Hello");
+            yield return new StatDrawEntry(StatCategoryDefOf.Apparel, "Covers".Translate(), "500", "Stat_Thing_Apparel_Covers_Desc".Translate(), 2750);
         }
 
         public static bool Prefix(CompArt __instance, ref TaggedString __result)
         {
             if (__instance.Title.EqualsIgnoreCase("Silence with Gartner"))
             {
-                __result = new TaggedString("An engraving on this furniture illustrates Gustav Gartner trying to light a fire while covered in frost. The scene takes place inside a snow-covered hickory forest. The scene takes place on the outskirts of a town. This artwork refers to Gartner freezing to death on 15th of Aprimay, 5500.");
+                __result =
+                    new
+                        TaggedString("An engraving on this furniture illustrates Gustav Gartner trying to light a fire while covered in frost. The scene takes place inside a snow-covered hickory forest. The scene takes place on the outskirts of a town. This artwork refers to Gartner freezing to death on 15th of Aprimay, 5500.");
                 return false;
             }
+
             return true;
         }
-    }
 
-    public class DummyDef : ThingDef
-    {
-        public DummyDef() : base()
+        public class DummyDef : ThingDef
         {
-            //HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.dummy_checker");
-            //harmony.Patch(AccessTools.Method(typeof(DefDatabase<ThingDef>), "AddAllInMods"), transpiler: new HarmonyMethod(typeof(Debug), nameof(Debug.Transpiler)));
-        }
+            public DummyDef() : base()
+            {
+                //HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.dummy_checker");
+                //harmony.Patch(AccessTools.Method(typeof(DefDatabase<ThingDef>), "AddAllInMods"), transpiler: new HarmonyMethod(typeof(Debug), nameof(Debug.Transpiler)));
+            }
 
 
-        public static void CheckDefName(ModMetaData __instance)
-        {
-            Log.Message(text: __instance.Name);
+            public static void CheckDefName(ModMetaData __instance)
+            {
+                Log.Message(text: __instance.Name);
+            }
         }
     }
 }
+*/
